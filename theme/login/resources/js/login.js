3230e120d9b03785e428fcaabab7a34e754b517c
Rob Ferguson <rob.ferguson@robferguson.org>
2020-04-17 21:56:38 +1000

    Updated the Login template

--- theme/login/resources/js/login.js
+++ theme/login/resources/js/login.js
@@ -2,51 +2,19 @@ window.onload = function() {
 
   console.log("login.js - onload()");
 
-  // add ripple effect to all material buttons on the page
+  // Add a ripple effect to all MDC buttons
   document.querySelectorAll('.mdc-button').forEach(function(e) {
     mdc.ripple.MDCRipple.attachTo(e);
   });
 
-  // initialize all text fields
+  // Initialize all MDC text fields
   document.querySelectorAll('.mdc-text-field').forEach(function(e) {
     new mdc.textField.MDCTextField(e);
   });
 
-  // initialize all icons
+  // Initialize all MDC text field icons
   document.querySelectorAll('.mdc-text-field__icon').forEach(function(e) {
     new mdc.textField.MDCTextFieldIcon(e);
   });
 
 };
-
-/*
-
-window.onload = function() {
-    // add ripple effect to all material buttons on the page
-    document.querySelectorAll('.mdc-button').forEach(function(e) {
-        mdc.ripple.MDCRipple.attachTo(e);
-    });
-
-    // initialize all text fields
-    document.querySelectorAll('.mdc-text-field').forEach(function(e) {
-        new mdc.textField.MDCTextField(e);
-    });
-
-    // initialize all icons
-    document.querySelectorAll('.mdc-text-field__icon').forEach(function(e) {
-        new mdc.textField.MDCTextFieldIcon(e);
-    });
-
-    // initialize the language select box
-    var select = new mdc.select.MDCSelect(
-        document.querySelector('.language-picker .mdc-select')
-    );
-
-    select.listen('change', function() {
-        var redirectUrl = document.querySelector('#language-picker-dropdown')
-            .value;
-        window.location.href = redirectUrl;
-    });
-};
-
-*/
