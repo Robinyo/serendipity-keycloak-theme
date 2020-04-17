03ed106e593caec319cf2001f39ce449797d8482
Rob Ferguson <rob.ferguson@robferguson.org>
2020-04-17 21:58:43 +1000

    Updated the Login template

--- theme/login/resources/js/login.js
+++ theme/login/resources/js/login.js
@@ -2,19 +2,19 @@ window.onload = function() {
 
   console.log("login.js - onload()");
 
-  // Add a ripple effect to all MDC buttons
-  document.querySelectorAll('.mdc-button').forEach(function(e) {
-    mdc.ripple.MDCRipple.attachTo(e);
-  });
-
-  // Initialize all MDC text fields
+  // Initialise all MDC text fields
   document.querySelectorAll('.mdc-text-field').forEach(function(e) {
     new mdc.textField.MDCTextField(e);
   });
 
-  // Initialize all MDC text field icons
+  // Initialise all MDC text field icons
   document.querySelectorAll('.mdc-text-field__icon').forEach(function(e) {
     new mdc.textField.MDCTextFieldIcon(e);
+  });
+
+  // Add a ripple effect to all MDC buttons
+  document.querySelectorAll('.mdc-button').forEach(function(e) {
+    mdc.ripple.MDCRipple.attachTo(e);
   });
 
 };
